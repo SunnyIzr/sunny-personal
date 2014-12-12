ba7e4406795af0b24085c414871b91fa00bdaa1f
Sunny Israni <sunny@zoraab.com>
2014-12-12 16:48:01 -0500

    perfecting portfolio--still buggy upon closing portfolio

--- scripts/portfolio.js
+++ scripts/portfolio.js
@@ -3,23 +3,38 @@ var Portfolio = {
     this.selectZoraab();
     this.selectMint();
     this.selectStats();
+    this.closePort();
   },
   selectZoraab: function(){
     $('#rm-zoraab .rm-cover .rm-front').click(function(e){
       e.preventDefault();
       PortfolioView.focusZoraab();
+      setTimeout( function(){
+        $('#rm-zoraab').addClass('rm-open')
+      }, 700)
     })
   },
   selectMint: function(){
     $('#rm-mint .rm-cover .rm-front').click(function(e){
       e.preventDefault();
       PortfolioView.focusMint();
+      setTimeout( function(){
+        $('#rm-mint').addClass('rm-open')
+      }, 700)
     })
   },
   selectStats: function(){
     $('#rm-stats .rm-cover .rm-front').click(function(e){
       e.preventDefault();
       PortfolioView.focusStats();
+      setTimeout( function(){
+        $('#rm-stats').addClass('rm-open')
+      }, 700)
+    })
+  },
+  closePort: function(){
+    $('.rm-container .close-icon svg').click(function(e){
+      PortfolioView.closeOpenSection();
     })
   }
 }
@@ -27,7 +42,15 @@ var Portfolio = {
 var PortfolioView = {
   clearFocus: function(){
     $('#portfolio .selected').removeClass('selected');
-    $('#portfolio .unselected').removeClass('unselected');
+    $('#portfolio .unselected-zoraab').removeClass('unselected-zoraab');
+    $('#portfolio .unselected-mint').removeClass('unselected-mint');
+    $('#portfolio .unselected-stats').removeClass('unselected-stats');
+  },
+  closeOpenSection: function(){
+    $('.rm-container.rm-open').removeClass('rm-open')
+    setTimeout(function(){
+      PortfolioView.clearFocus();
+    }, 700)
   },
   focusMint: function(){
     $('#rm-zoraab').addClass('unselected-mint');
